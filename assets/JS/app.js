3a61f0141d3b6e49c209a172580dc8375ec7ee4d
Sultan Karimov <skarimovkgz@gmail.com>
2019-11-15 00:07:21 -0600

    final commit

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){
+
+
+$(document).ready(function () {
     var firebaseConfig = {
         apiKey: "AIzaSyBn9OMKOTlSnuIYYmA_zScQKJHcgLqMZ2Y",
         authDomain: "pr-1-aa3ea.firebaseapp.com",
@@ -13,8 +15,6 @@ $(document).ready(function(){
 
    
     var database = firebase.database();
-
-    // Vars for onClick event
     var name;
     var destination;
     var firstTrain;
@@ -22,13 +22,11 @@ $(document).ready(function(){
 
     $("#add-train").on("click", function() {
         event.preventDefault();
-        // Storing and retreiving new train data
         name = $("#train-name").val().trim();
         destination = $("#destination").val().trim();
         firstTrain = $("#first-train").val().trim();
         frequency = $("#frequency").val().trim();
 
-        // Pushing to database
         database.ref().push({
             name: name,
             destination: destination,
@@ -42,14 +40,10 @@ $(document).ready(function(){
     database.ref().on("child_added", function(childSnapshot) {
         var nextArr;
         var minAway;
-        // Chang year so first train comes before now
         var firstTrainNew = moment(childSnapshot.val().firstTrain, "hh:mm").subtract(1, "years");
-        // Difference between the current and firstTrain
         var diffTime = moment().diff(moment(firstTrainNew), "minutes");
         var remainder = diffTime % childSnapshot.val().frequency;
-        // Minutes until next train
         var minAway = childSnapshot.val().frequency - remainder;
-        // Next train time
         var nextTrain = moment().add(minAway, "minutes");
         nextTrain = moment(nextTrain).format("hh:mm");
 
@@ -58,14 +52,11 @@ $(document).ready(function(){
                 "</td><td>" + childSnapshot.val().frequency +
                 "</td><td>" + nextTrain + 
                 "</td><td>" + minAway + "</td></tr>");
-
-            // Handle the errors
         }, function(errorObject) {
             console.log("Errors handled: " + errorObject.code);
     });
 
     database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
-        // HTML to reflect
         $("#name-display").html(snapshot.val().name);
         $("#email-display").html(snapshot.val().email);
         $("#age-display").html(snapshot.val().age);
